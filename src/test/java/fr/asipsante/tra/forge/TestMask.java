3309a208599c480eb173b9dc729c2c3e40147bad
mjuravliov <mjuravliov@f52e61b7-f110-4703-9dac-908fd50f7d42>
2015-12-10 12:22:55 +0000

    Compilable test.

---
+++ src/test/java/fr/asipsante/tra/forge/TestMask.java
@@ -0,0 +1,12 @@
+package fr.asipsante.tra.forge;
+
+public class TestMask
+{
+protected int xmlPath ;
+
+	public void setXmlPath(int xmlPath)
+	{
+		//xmlPath.toString()
+		this.xmlPath = xmlPath ;
+	}
+}
