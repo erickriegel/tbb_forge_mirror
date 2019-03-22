a09b3434d303fb6787474aa1e3073b7b256758be
kguilloux <kguilloux@f52e61b7-f110-4703-9dac-908fd50f7d42>
2019-03-22 15:40:07 +0000

    Test règle Sonar "Statements should be on separate lines"

--- src/main/java/fr/asipsante/tra/forge/App.java
+++ src/main/java/fr/asipsante/tra/forge/App.java
@@ -8,8 +8,17 @@ import org.apache.commons.beanutils.converters.DateConverter;
  
  public class App 
 {
+	String test1 test2;
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World! "  + new DateConverter().toString());
+		test1 = true;
+		
+		doSomething(){
+			System.out.println("Coucou");
+		}
+		
+		if (test1) doSomething();
     }
 }
