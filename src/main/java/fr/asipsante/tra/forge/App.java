119de6b88caae99c05ba8a0fec1ec10dcb069d3e
kguilloux <kguilloux@f52e61b7-f110-4703-9dac-908fd50f7d42>
2019-03-25 09:03:40 +0000

    Corrections

--- src/main/java/fr/asipsante/tra/forge/App.java
+++ src/main/java/fr/asipsante/tra/forge/App.java
@@ -8,10 +8,16 @@ import org.apache.commons.beanutils.converters.DateConverter;
  
  public class App 
 {
-
+	public static boolean test1, test2;
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! "  + new DateConverter().toString());
+    	System.out.println( "Hello World! "  + new DateConverter().toString());
+		test1 = true;
+		if (test1) doSomething();
     }
-
+	
+	public static void doSomething(){
+		System.out.println("Coucou");
+	};
 }
