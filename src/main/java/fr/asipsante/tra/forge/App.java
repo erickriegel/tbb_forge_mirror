efa57fda1229ab0a51ff4062dd62e45d54ee016b
RhodeCode Admin <admin@example.com>
2022-10-25 14:45:52 +0000

    Editer le fichier src/main/java/fr/asipsante/tra/forge/App.java via RhodeCode Enterprise

--- src/main/java/fr/asipsante/tra/forge/App.java
+++ src/main/java/fr/asipsante/tra/forge/App.java
@@ -3,42 +3,11 @@ package fr.asipsante.tra.forge;
 /**
  * Hello world!
  *
- *
  */
-import org.apache.commons.beanutils.converters.DateConverter;
- 
- public class App 
+public class App 
 {
-	public static boolean test1, test2;
-	public String variablePublique = "publique";
-	public String myvar = "test";
-	private String PASSWORD = "password";
-	private String PASSWORD2 = "password";
-	private String PASSWORD3 = "password";
-	
     public static void main( String[] args )
     {
-    	System.out.println( "Hello World! "  + new DateConverter().toString());
-		test1 = true;
-		test2 = false;
-		if (test1) {
-		   doSomething();
-		   test2 = true;
-		}
+        System.out.println( "Hello World!" );
     }
-	
-	public static void doSomething(){
-		System.out.println("Coucou");
-	};
-	
-	public String doSomething2(){
-		String mot1 = "test", mot2 = "retest";
-		int chiffre1, chiffre2;
-		chiffre1 = 1;
-		try {
-		}
-		catch (Exception e){
-		}
-		return (mot1);
-	};
 }
